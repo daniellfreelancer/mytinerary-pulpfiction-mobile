f1a10be1132ce185fcf57d7ff23314a498b7890e
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-09-27 16:07:16 -0300

    update navigation

--- src/Navigation/Stack.js
+++ src/Navigation/Stack.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Cities from '../components/Cities'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const StackNavigator = createNativeStackNavigator()
 
@@ -10,9 +11,10 @@ const StackNavigator = createNativeStackNavigator()
 export default function Stack() {
   return (
     <StackNavigator.Navigator>
+
         <StackNavigator.Screen name='Home' component={Hero} options={{ title: 'Welcome to Pulp-Fiction Travels' }} />
         <StackNavigator.Screen name='Cities' component={Cities} />
-        
+
     </StackNavigator.Navigator>
   )
 }
